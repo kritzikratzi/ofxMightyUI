d940404fdc555b2422e66a4831992dafc2271a9d
Hansi Raber <super@superduper.org>
2016-04-22 19:34:36 +0200

    moved segmented select to header file, the other thing was impractical

--- src/SegmentedSelect.h
+++ src/SegmentedSelect.h
@@ -14,12 +14,16 @@
 
 #include "MuiCore.h"
 #include "Button.h"
+#include "Label.h"
 
 namespace mui{
 	template<typename T>
 	class SegmentedButton : public Button{
 	public: 
-		SegmentedButton( std::string title_, T value, float x_ = 0, float y_ = 0, float width_ = 200, float height_ = 30 );
+		SegmentedButton( std::string title_, T value, float x_ = 0, float y_ = 0, float width_ = 200, float height_ = 30 ) : Button( title_, x_, y_, width_, height_ ), selected(false), value(value) {
+			label->fontSize -= 2;
+			label->commit();
+		}
 		
 		bool selected;
 		bool roundedLeft; 
@@ -27,37 +31,105 @@ namespace mui{
 		T value;
 		
 		
-		virtual void initSegmentedButton(); 
-		virtual void drawBackground();
+		virtual void drawBackground(){
+			ofSetColor( 255, 255, 255 );
+			Helpers::beginImages();
+			if( selected || pressed ){
+				if( roundedLeft ) Helpers::drawImage( "segment_left_active", 0, 0, 5, 29 );
+				Helpers::drawImage( "segment_center_active", roundedLeft?5:0, 0, width-((roundedLeft?5:0)+(roundedRight?5:0)), 29 );
+				if( roundedRight ) Helpers::drawImage( "segment_right_active", width-5, 0, 5, 29 );
+			}
+			else{
+				if( roundedLeft ) Helpers::drawImage( "segment_left", 0, 0, 5, 29 );
+				Helpers::drawImage( "segment_center", roundedLeft?5:0, 0, width-((roundedLeft?5:0)+(roundedRight?5:0)), 29 );
+				if( roundedRight ) Helpers::drawImage( "segment_right", width-5, 0, 5, 29 );
+			}
+			
+			if( !roundedRight ){
+				Helpers::drawImage( "segment_separator", width-1, 0, 1, 29 );
+			}
+			Helpers::endImages();
+		}
 	};
 	
 	template<typename T>
 	class SegmentedSelect : public Container{
 	public: 
-		SegmentedSelect( float x_ = 0, float y_ = 0, float width_ = 200, float height_ = 20 );
+		SegmentedSelect( float x_ = 0, float y_ = 0, float width_ = 200, float height_ = 20 ): Container( x_, y_, width_, height_ ), selected(NULL){
+		}
 		
 		SegmentedButton<T> * selected;
 		
-		virtual void addSegment( string text, T value );
-		virtual void commit();
+		virtual void addSegment( string text, T value ){
+			SegmentedButton<T> * button = new SegmentedButton<T>( text, value );
+			button->width = button->label->boundingBox.width + 10;
+			ofAddListener(button->onPress, this, &SegmentedSelect::onButtonPress );
+			
+			add( button );
+			commit();
+		}
+		
+		virtual void commit(){
+			vector<Container*>::iterator it = children.begin();
+			int x = 0;
+			bool first = true;
+			SegmentedButton<T> * last = NULL;
+			
+			while( it != children.end() ){
+				SegmentedButton<T> * button = (SegmentedButton<T>*) *it;
+				button->x = x;
+				x += button->width;
+				button->selected = button == selected;
+				button->roundedLeft = first;
+				button->roundedRight = false;
+				
+				first = false;
+				last = button;
+				
+				++it;
+			}
+			
+			if( last != NULL ){
+				last->roundedRight = true; 
+			}
+		}
 
 		// iterator?
-		virtual size_t getNumSegments();
-		virtual SegmentedButton<T> * getSegment( int num );
-		
-		virtual void update();
-		virtual void draw();
-		virtual void drawBackground();
-		
-		virtual void touchDown( ofTouchEventArgs &touch );
-		virtual void touchMoved( ofTouchEventArgs &touch ); 
-		virtual void touchUp( ofTouchEventArgs &touch ); 
-		virtual void touchDoubleTap( ofTouchEventArgs &touch ); 
+		virtual size_t getNumSegments(){
+			return children.size();
+		};
+		virtual SegmentedButton<T> * getSegment( int num ){
+			mui::SegmentedButton<T> * button = (mui::SegmentedButton<T>*)children[num];
+			return button;
+		}
+		virtual SegmentedButton<T> * setSelected( const T value ){
+			for( mui::Container * c : children ){
+				mui::SegmentedButton<T> * button = (mui::SegmentedButton<T>*)c;
+				if( button->value == value ){
+					selected = button;
+					commit();
+					return button;
+				}
+			}
+			return selected;
+		}
+		virtual SegmentedButton<T> * setSelectedAndNotify( const T value ){
+			setSelected(value);
+			if( selected != NULL ) selected->clickAndNotify();
+			return selected;
+		}
 
 		ofEvent<SegmentedButton<T>*> onChange;
+		ofEvent<T> onChangeValue;
 		
 	private: 
-		virtual void onButtonPress( const void* sender, ofTouchEventArgs &args );
+		virtual void onButtonPress( const void* sender, ofTouchEventArgs &args ){
+			SegmentedButton<T> * button = (SegmentedButton<T>*) sender;
+			selected = button;
+			ofNotifyEvent(onChange, selected, this);
+			ofNotifyEvent(onChangeValue, selected->value, this);
+			commit();
+		}
 	};
 };
 
